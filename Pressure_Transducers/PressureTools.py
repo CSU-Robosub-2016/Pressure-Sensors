a2d8cf97068741376bb68137af6c9effde394347
bwgonzal <bwgonzal@rams.colostate.edu>
2017-03-22 09:06:11 -0600

    Update PressureTools.py

--- Pressure_Transducers/PressureTools.py
+++ Pressure_Transducers/PressureTools.py
@@ -5,5 +5,3 @@ import I2C.pressuretrans
 
 i2c = I2C.BusI2C('COM2')
 i2c.bus.setSpeed(2000)  # you can set i2c speed adapted to your hardware
-sonde = I2C.pressuretrans.LM75('Pressure Voltage', i2c)
-print "T =  %02.03f C" % sonde.getTemperature()
